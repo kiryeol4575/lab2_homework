dc0e1bc8b4be8fb3f4f52ee3e4e4182b75fb35f6
kiryeol4575 <sam21018@gmail.com>
2023-09-05 20:11:55 +0900

    modify count.cc

--- count.cc
+++ count.cc
@@ -1,6 +1,8 @@
 #include "count.h"
 
 void count_number() {
-    // lab2 //
-    // do it yourself //
+    using namespace std;
+
+    for(int i=1; i<=10; i++) cout << i << endl;
+    cout << "2019312752" << endl;
 }
